de1b3d12624b865ed9b1cb3fdd33a73b38d1cc4b
idsc3101 <idsc3101@gmail.com>
2014-10-08 15:08:15 -0500

    work done in class

--- DoWhileDemo/DoWhileDemo/Module1.vb
+++ DoWhileDemo/DoWhileDemo/Module1.vb
@@ -6,10 +6,18 @@ Module Module1
         Dim num As Integer = 1
 
         Do While num <= 7
+            'if we were to add num=10 here, it will still complete 1 loop and then end because num=11 which is >7 
+
+            If num > 5 Then
+                Exit Do
+            End If
+            'when num=5, it will exit the loop 
+            'if num=num+1 was written before Console.WriteLine then num=6 will allow exit from loop
             Console.WriteLine(" " & num)
             num = num + 1
         Loop
-
+        'if we write the Console.WriteLine here, then num would be 8 because the only way it could leave the loop 
+        'would be if it was >7 
     End Sub
 
 End Module
--- PopulationYear/PopulationYear/Form1.vb
+++ PopulationYear/PopulationYear/Form1.vb
@@ -3,7 +3,17 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As Object, e As EventArgs) Handles btnPopulation.Click
-        MessageBox.Show("The answer: not implemented yet!")
+        Dim year As Integer = 2014
+        Dim population As Double = 7000
+
+        Do While (population >= 6)
+            year = year - 50
+            population = population / 2
+
+
+        Loop
+
+        MessageBox.Show("The year is " & year)
 
     End Sub
 End Class
